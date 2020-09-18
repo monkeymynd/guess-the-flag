0aebe1558d0c2fbc0963df3b00b13987678e5773
Stacey R. Brodsky <monkeymynd@gmail.com>
2020-09-18 06:57:16 -0500

    Updates

---
+++ precache-manifest.2f9493173cb2f0ea7cabf65f23075930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549a4c095eb1b2a695dfb8c4e20f37cf",
+    "url": "/guess-the-flag/index.html"
+  },
+  {
+    "revision": "f10f567e0c99570993a6",
+    "url": "/guess-the-flag/static/css/main.cae9d934.chunk.css"
+  },
+  {
+    "revision": "bc0f1e9a91343e2be124",
+    "url": "/guess-the-flag/static/js/2.42151572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guess-the-flag/static/js/2.42151572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10f567e0c99570993a6",
+    "url": "/guess-the-flag/static/js/main.50a63005.chunk.js"
+  },
+  {
+    "revision": "7f0b94079c694e99ee9b",
+    "url": "/guess-the-flag/static/js/runtime-main.5e84e397.js"
+  }
+]);
